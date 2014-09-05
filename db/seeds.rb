8ae79f5bddb9292bc844f3262e05549b9fc6ef77
Puru Dahal <pdahal@me.com>
2014-09-04 18:17:09 -0700

    Add dictionary words and convert it to json.
    Add mac dictionary words to words.txt
    Convert words.txt to words.json
    Arrange them in alphabetical order

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require "json"
+
+
+class Domainify
+
+  def initialize
+    @file = 'words.csv'
+    @json_words = Hash.new()
+    @array_of_words = Array.new()
+  end
+
+  def jsonify
+    letters = ("a".."z").to_a
+    words_hash = Hash[letters.map {|x| [x.to_s, Array.new]}]
+
+    ` if [ ! -f words.csv ]; then
+    cat /usr/share/dict/words | tee words.csv
+    fi `
+    
+    File.foreach(@file)  do |line|
+      word = line.chomp.downcase
+      words_hash[word[0]] << word if word.length >= 2
+    end
+
+    File.open("words.json","w").write(words_hash)    
+  end
+
+end
+
+words = Domainify.new
+words.jsonify
