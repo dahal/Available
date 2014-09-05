559ab3c36a14779ad83b4a3b0325fbee65203f67
Puru Dahal <pdahal@me.com>
2014-09-04 19:19:42 -0700

    Add three letter and four letter words

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,30 @@
 require "json"
 
-
 class Domainify
 
   def initialize
-    @file = 'words.csv'
+    @file = 'fixtures/dictionary_words.csv'
     @json_words = Hash.new()
     @array_of_words = Array.new()
+  end
+
+  def dictionary_words
+    ` if [ ! -f fixtures/dictionary_words.csv ]; then
+    cat /usr/share/dict/words | tee words.csv
+    fi `   
   end
 
   def jsonify
     letters = ("a".."z").to_a
     words_hash = Hash[letters.map {|x| [x.to_s, Array.new]}]
-
-    ` if [ ! -f words.csv ]; then
-    cat /usr/share/dict/words | tee words.csv
-    fi `
     
     File.foreach(@file)  do |line|
       word = line.chomp.downcase
-      words_hash[word[0]] << word if word.length >= 2
+      words_hash[word[0]] << word if word.length >= 2 && word.length <= 4
+      words_hash[word[0]].uniq!
     end
 
-    File.open("words.json","w").write(words_hash)    
+    File.open("dictionary_words.json","w").write(words_hash)    
   end
 
 end
