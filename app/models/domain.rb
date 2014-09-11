370bac041a7053db6a68a22df87c88fda1b13f83
Arik Gadye <arikdovgadye@gmail.com>
2014-09-11 14:57:53 -0700

    fix error handling

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,14 +1,4 @@
 class Domain
 	include MongoMapper::Document
-	before_create :check_availability
-
-	private
-	def check_availability
-		name = self.name
-		begin
-			Whois.whois(name).available? == true ? true : false
-		rescue
-			false
-		end
-	end  
+	validates :name, uniqueness: true
 end
