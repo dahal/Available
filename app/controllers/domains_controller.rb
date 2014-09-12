07fba7126038072e9460a0a2519beba210b2899a
Puru Dahal <pdahal@me.com>
2014-09-11 18:43:47 -0700

    Render json objects with three_letter_domains & four_letter_domains
    TDD DomainsController#three_letter_domains
    TDD DomainsController#four_letter_domains

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -4,5 +4,15 @@ class DomainsController < ApplicationController
     @domains = Domain.all.first(100)
     render json: @domains.to_json
   end
-  
+
+  def three_letter_domains
+    @domains = Domain.all.select {|domain| domain.name.split('.')[0].length == 3 }
+    render json: @domains.to_json
+  end
+
+  def four_letter_domains
+    @domains = Domain.all.select { |domain| domain.name.split('.')[0].length == 4 }
+    render json: @domains.to_json
+  end
+
 end
--- spec/controllers/domains_controller_spec.rb
+++ spec/controllers/domains_controller_spec.rb
@@ -1,13 +1,34 @@
 require 'rails_helper'
 
 describe DomainsController do
-  let!(:sample_domains) { 100.times { Domain.create(name: ("a".."z").to_a.sample(3).join()+'.academy') } } 
+  before(:each) do
+    Domain.delete_all
+  end
   context "index" do
+    let!(:sample_domains) { 100.times { Domain.create(name: ("a".."z").to_a.sample(3).join()+'.academy') } } 
     it "Should list 100 ramdom lists of domains" do
       get :index
-      expect(response).to  be_success
+      expect(response).to be_success
       json = JSON.parse(response.body)
       expect(json.count).to eq(100)
+    end
+  end
+
+  context "#three_letter_domains" do
+    let!(:awesome_domains) { 100.times { Domain.create(name: ("a".."z").to_a.sample(3).join()+'.academy') } }
+    it "Should list available three_letter_domain names" do
+      get :three_letter_domains
+      domain_length = JSON.parse(response.body).first["name"].split('.').first.length
+      expect(domain_length).to eq(3) 
+    end
+  end
+
+  context "#four_letter_domains" do
+    let!(:awesome_domains) { 100.times { Domain.create(name: ("a".."z").to_a.sample(4).join()+'.academy') } }
+    it "Should list available four_letter_domain names" do
+      get :four_letter_domains
+      domain_length = JSON.parse(response.body).first["name"].split('.').first.length
+      expect(domain_length).to eq(4) 
     end
   end
 end
